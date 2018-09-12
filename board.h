4f2d5a34d8db2856e5874bb61518e5ad12cfc6af
cdgraham <chris@cdgraham.com>
2018-09-12 07:31:36 +0100

    Create board.h

---
+++ board.h
@@ -0,0 +1,14 @@
+#ifndef _BOARD_H_
+#define _BOARD_H_
+
+#include <stdio.h>
+
+enum color {
+        EMPTY,
+        BLACK,
+        WHITE
+};
+
+#define MAX_BOARD       19      /* Maximum board size */
+
+#endif  /* _BOARD_H_ */
